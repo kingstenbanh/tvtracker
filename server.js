8e5c06cc99bfa0b34268e1a2dedb34f77a583ed5
Kingsten Banh <kingsten.banh@gmail.com>
2015-02-05 11:18:00 -0700

    remove agenda

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ var async = require('async');
 var request = require('request');
 var xml2js = require('xml2js');
 
-var agenda = require('agenda')({ db: { address: 'mongodb://kingstenbanh:fantasy1@ds041651.mongolab.com:41651/tvtracker
-'} });
+// var agenda = require('agenda')({ db: { address: 'mongodb://kingstenbanh:fantasy1@ds041651.mongolab.com:41651/tvtracker
+// '} });
 var sugar = require('sugar');
 var nodemailer = require('nodemailer');
 var _ = require('lodash');
@@ -355,49 +355,49 @@ app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'));
 });
 
-agenda.define('send email alert', function(job, done) {
-  Show.findOne({ name: job.attrs.data }).populate('subscribers').exec(function(err, show) {
-    var emails = show.subscribers.map(function(user) {
-      if (user.facebook) {
-        return user.facebook.email;
-      } else if (user.google) {
-        return user.google.email
-      } else {
-        return user.email
-      }
-    });
-
-    var upcomingEpisode = show.episodes.filter(function(episode) {
-      return new Date(episode.firstAired) > new Date();
-    })[0];
-
-    var smtpTransport = nodemailer.createTransport('SMTP', {
-      service: 'SendGrid',
-      auth: { user: 'hslogin', pass: 'hspassword00' }
-    });
-
-    var mailOptions = {
-      from: 'Fred Foo ✔ <foo@blurdybloop.com>',
-      to: emails.join(','),
-      subject: show.name + ' is starting soon!',
-      text: show.name + ' starts in less than 2 hours on ' + show.network + '.\n\n' +
-        'Episode ' + upcomingEpisode.episodeNumber + ' Overview\n\n' + upcomingEpisode.overview
-    };
-
-    smtpTransport.sendMail(mailOptions, function(error, response) {
-      console.log('Message sent: ' + response.message);
-      smtpTransport.close();
-      done();
-    });
-  });
-});
-
-//agenda.start();
-
-agenda.on('start', function(job) {
-  console.log("Job %s starting", job.attrs.name);
-});
-
-agenda.on('complete', function(job) {
-  console.log("Job %s finished", job.attrs.name);
-});
+// agenda.define('send email alert', function(job, done) {
+//   Show.findOne({ name: job.attrs.data }).populate('subscribers').exec(function(err, show) {
+//     var emails = show.subscribers.map(function(user) {
+//       if (user.facebook) {
+//         return user.facebook.email;
+//       } else if (user.google) {
+//         return user.google.email
+//       } else {
+//         return user.email
+//       }
+//     });
+//
+//     var upcomingEpisode = show.episodes.filter(function(episode) {
+//       return new Date(episode.firstAired) > new Date();
+//     })[0];
+//
+//     var smtpTransport = nodemailer.createTransport('SMTP', {
+//       service: 'SendGrid',
+//       auth: { user: 'hslogin', pass: 'hspassword00' }
+//     });
+//
+//     var mailOptions = {
+//       from: 'Fred Foo ✔ <foo@blurdybloop.com>',
+//       to: emails.join(','),
+//       subject: show.name + ' is starting soon!',
+//       text: show.name + ' starts in less than 2 hours on ' + show.network + '.\n\n' +
+//         'Episode ' + upcomingEpisode.episodeNumber + ' Overview\n\n' + upcomingEpisode.overview
+//     };
+//
+//     smtpTransport.sendMail(mailOptions, function(error, response) {
+//       console.log('Message sent: ' + response.message);
+//       smtpTransport.close();
+//       done();
+//     });
+//   });
+// });
+//
+// //agenda.start();
+//
+// agenda.on('start', function(job) {
+//   console.log("Job %s starting", job.attrs.name);
+// });
+//
+// agenda.on('complete', function(job) {
+//   console.log("Job %s finished", job.attrs.name);
+// });
